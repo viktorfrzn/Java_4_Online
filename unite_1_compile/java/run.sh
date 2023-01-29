e117c919bae5653efaf9910ff6d301e8f0555e06
home pc <viktordemydenko9519@gmail.com>
2023-01-29 15:57:08 +0300

    add run.sh

--- unite_1_compile/java/run.sh
+++ unite_1_compile/java/run.sh
@@ -1,30 +1,32 @@
+#!/bin/sh
+
 echo 'run simple'
 cd ./simple
 javac Hello.java
 java Hello
 
-cd
+cd../
 
 echo 'run package'
 cd ./package
 javac ua/com/alevel/Hello.java
 java ua.com.alevel.Hello
 
-cd
+cd../
 
 echo 'run separate_new_package'
 cd. /separate_new_package
 javac ua/com/alevel/Hello.java
 java ua.com.alevel.Hello
 
-cd
+cd../
 
 echo 'run minimal proj'
 cd. /minimal_proj
 javac -sourcepath ./src -d build/classes ./src/ua/com/alevel/Hello.java
 java -cp build/classes ua.com.alevel.Hello
 
-cd
+cd../
 
 echo 'run med proj amd simple jar'
 cd. /med_proj
