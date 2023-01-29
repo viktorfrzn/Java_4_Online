54787f35cde1e576960d2224cbd2605a391df13b
home pc <viktordemydenko9519@gmail.com>
2023-01-29 17:35:35 +0300

    add unite_2_about_class

---
+++ unite_2_about_class/src/main/java/ua/com/alevel/Human.java
@@ -0,0 +1,16 @@
+package ua.com.alevel;
+
+public class Human {
+    String name;
+    int age;
+
+
+    void drink()
+    {
+        System.out.println("Drink coca cola");
+
+    }
+    String info() {
+        return "name" + name + "age" + age;
+    }
+}
--- unite_2_about_class/src/main/java/ua/com/alevel/Main.java
+++ unite_2_about_class/src/main/java/ua/com/alevel/Main.java
@@ -3,5 +3,7 @@ package ua.com.alevel;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Human Human = new Human();
+        Human.drink();
     }
 }
