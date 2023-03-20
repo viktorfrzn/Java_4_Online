13ec1f341236c8938066f09e27eab49eb6058928
Виктор <viktordemydenko9519@gmail.com>
2023-03-20 20:50:34 +0200

    unite_4_about_oop

--- unite_4_about_oop/src/main/java/ua/com/alevel/ClientInfo.java
+++ unite_4_about_oop/src/main/java/ua/com/alevel/ClientInfo.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class ClientInfo {
     public void start() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Welcome to Discaunt Data Base");
+        System.out.println("Welcome to Data Base");
         String select;
         menu();
         while ((select = reader.readLine()) != null) {
@@ -21,7 +21,7 @@ public class ClientInfo {
         System.out.println("Add to Client Data Base Enter 1");
         System.out.println("Show all Clients of This Store Enter 2");
         System.out.println("Find The Client by Email Enter 3");
-        System.out.println("Delete User From Data Base Enter 4");
+        System.out.println("Delete User From Base Enter 4");
         System.out.println("Exit Enter 0");
         System.out.println();
     }
